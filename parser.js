9d1125e56792481cfc8da5c2f3ef7f11f0b91f8f
syed-imam <syedadilimam93@gmail.com>
2022-03-09 16:10:27 -0500

    Cleanup

--- parser.js
+++ parser.js
@@ -4,16 +4,15 @@ import blockTypes from './block-types'
 
 const blockMap = new Map()
 
-// TODO: Video node in the body
-blockMap.set(blockTypes.PARAGRAPH, paragraphResolver) //Done
-blockMap.set(blockTypes.HEADER, headerResolver) //Done
-blockMap.set(blockTypes.LIST, listResolver) //Done
-blockMap.set(blockTypes.IMAGE, imageResolver) //Done
-blockMap.set(blockTypes.EMBED, embedResolver) //In progress
-blockMap.set(blockTypes.TWITTER, twitterResolver) //Done
-blockMap.set(blockTypes.QUOTE, quoteResolver) //Done
-blockMap.set(blockTypes.SPOT_IM, spotimResolver) //Done
-blockMap.set(blockTypes.TAGBOARD, tagboardResolver)//Done
-blockMap.set(blockTypes.VIDEO, videoResolver) //Done
+blockMap.set(blockTypes.PARAGRAPH, paragraphResolver)
+blockMap.set(blockTypes.HEADER, headerResolver)
+blockMap.set(blockTypes.LIST, listResolver)
+blockMap.set(blockTypes.IMAGE, imageResolver)
+blockMap.set(blockTypes.EMBED, embedResolver)
+blockMap.set(blockTypes.TWITTER, twitterResolver)
+blockMap.set(blockTypes.QUOTE, quoteResolver)
+blockMap.set(blockTypes.SPOT_IM, spotimResolver)
+blockMap.set(blockTypes.TAGBOARD, tagboardResolver)
+blockMap.set(blockTypes.VIDEO, videoResolver)
 
 export default blockMap
