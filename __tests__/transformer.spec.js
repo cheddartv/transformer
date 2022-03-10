c5394018dc1f9bbb7fb51ff6b06546583514ff2a
syed-imam <syedadilimam93@gmail.com>
2022-03-10 01:25:37 -0500

    Fix test

--- __tests__/transformer.spec.js
+++ __tests__/transformer.spec.js
@@ -1,12 +1,15 @@
+import fs from 'fs'
+
 import blocks from '../mocks/article.json'
+import formatString from '../util/formatString'
 
 import Transformer from '../transformer'
 
 describe('Transformer', () => {
   it('should parse blocks correctly', () => {
+    const expectedHtml = fs.readFileSync(__dirname + '/../mocks/html/article.html', 'utf8')
     const transformer = new Transformer()
     const parsedString = transformer.parse(blocks)
-    console.log(parsedString)
-    //expect(parsedString).toEqual(expectedResult)
+    expect(formatString(parsedString)).toEqual(formatString(expectedHtml))
   })
 })
