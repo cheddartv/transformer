cc46039a04115f7f4bb82d8c0b83c9b57494fa44
syed-imam <syedadilimam93@gmail.com>
2022-03-11 14:25:32 -0500

    Check in dist folder

---
+++ dist/embed-types.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var embedTypes = {
+  YOUTUBE: 'youtube',
+  CHEDDAR_VIDEO: 'cheddar',
+  FACEBOOK: 'facebook',
+  UGC_CAROUSEL: 'ugc_carousel',
+  UGC_FORM: 'ugc_form'
+};
+var _default = embedTypes;
+exports["default"] = _default;
---
+++ dist/resolvers/facebook.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _default = function _default(data, config) {
+  return "\n    <div class=\"".concat(config.embed["class"], "\" style=\"width: ").concat(data.width, "px;\">\n      <embed class=\"").concat(config.embed["class"], "\" height=\"").concat(data.height, "\"\n        width=\"").concat(data.width, "\" src=\"").concat(data.embed, "\">\n      <em>").concat(data.caption, "</em>\n    </div>");
+};
+
+exports["default"] = _default;
