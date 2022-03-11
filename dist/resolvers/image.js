7e8f48caf27c68da1d68f7864331a01471fb0967
syed-imam <syedadilimam93@gmail.com>
2022-03-11 13:48:49 -0500

    Fix default export

---
+++ dist/resolvers/image.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _default = function _default(data, config) {
+  return "<div>\n <img src=\"".concat(data.file.url, "\" alt=\"\"/>\n <em>").concat(data.caption, "</em>\n</div>\n");
+};
+
+exports["default"] = _default;
