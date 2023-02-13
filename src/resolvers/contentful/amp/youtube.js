576acaebce79ea3ae130a83448d3c94522e6710f
ignatospadov <tigeroip45@gmail.com>
2023-02-13 08:41:36 -0500

    fixed spacing

--- src/resolvers/contentful/amp/youtube.js
+++ src/resolvers/contentful/amp/youtube.js
@@ -1,6 +1,4 @@
 import { extractYoutubeVideoId } from '../common'
 
 export default (node) =>
-  `<amp-youtube data-videoid="${extractYoutubeVideoId(node.content)}" layout="responsive" width="${node.attributes.width}" height="${
-    node.attributes.height
-  }"></amp-youtube>`
+  `<amp-youtube data-videoid="${extractYoutubeVideoId(node.content)}" layout="responsive" width="${node.attributes.width}" height="${node.attributes.height}"></amp-youtube>`
