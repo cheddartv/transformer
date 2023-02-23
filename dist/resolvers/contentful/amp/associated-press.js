a76bb737c58fcec6f09e7edf5b9c45a2ef24be72
ignatospadov <tigeroip45@gmail.com>
2023-02-23 14:17:53 -0500

    fixed height for embed for AP

--- dist/resolvers/contentful/amp/associated-press.js
+++ dist/resolvers/contentful/amp/associated-press.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 exports["default"] = void 0;
 
 var _default = function _default(node) {
-  return "\n  <amp-iframe\n    layout=\"responsive\"\n    width=\"300\"\n    height=\"100\"\n    sandbox=\"allow-scripts allow-same-origin\"\n    src=\"".concat(node.content, "\">\n    <amp-img placeholder\n      src=\"/loading-animation.gif\"\n      height=\"64\"\n      width=\"64\"\n      layout=\"fixed\">\n    </amp-img>\n  </amp-iframe>");
+  return "\n  <amp-iframe\n    layout=\"responsive\"\n    width=\"300\"\n    height=\"300\"\n    sandbox=\"allow-scripts allow-same-origin\"\n    src=\"".concat(node.content, "\">\n    <amp-img placeholder\n      src=\"/loading-animation.gif\"\n      height=\"64\"\n      width=\"64\"\n      layout=\"fixed\">\n    </amp-img>\n  </amp-iframe>");
 };
 
 exports["default"] = _default;
