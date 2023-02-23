52f322f347b2fc506e5e00a7a73ba9e3dd4fb3e0
ignatospadov <tigeroip45@gmail.com>
2023-02-23 08:59:00 -0500

    updated google maps

--- src/resolvers/contentful/google-maps.js
+++ src/resolvers/contentful/google-maps.js
@@ -4,16 +4,12 @@ export default (node) => {
   const src = node.content.match(/<iframe.*?\bsrc="(.*?)".*?>/i)
 
   if (src) {
-    const height = node.content.match(/<iframe.*?\bheight="(.*?)".*?>/i) ? node.content.match(/<iframe.*?\bheight="(.*?)".*?>/i)[1] : '450'
-    const width = node.content.match(/<iframe.*?\bwidth="(.*?)".*?>/i) ? node.content.match(/<iframe.*?\bwidth="(.*?)".*?>/i)[1] : '600'
-
     return `
       <div class="${config.embed.class} ${config.embed['google-maps'].class}">
         <iframe
           src="${src[1]}"
-          width="${width}"
-          height="${height}"
-          style="border:0;"
+          height="400"
+          style="border:0;width:100%;"
           allowfullscreen=""
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade">
