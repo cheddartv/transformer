6918726f08eb0047ab55bec6067d32b5fcbe1e29
ignatospadov <tigeroip45@gmail.com>
2023-02-23 12:13:49 -0500

    fixed pdf iframe

--- dist/resolvers/contentful/pdf.js
+++ dist/resolvers/contentful/pdf.js
@@ -10,7 +10,7 @@ var _config = _interopRequireDefault(require("../../config"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var _default = function _default(node) {
-  return "<div\n    class=\"".concat(_config["default"].asset["class"], "\"\n    data-type=\"").concat(node.data.target.fields.file.contentType, "\"\n    data-src=\"https:").concat(node.data.target.fields.file.url, "\">\n  </div>");
+  return "<div class=\"".concat(_config["default"].embed["class"], " ").concat(_config["default"].asset["class"], "\">\n    <iframe\n      src=\"https://docs.google.com/viewerng/viewer?url=https:").concat(node.data.target.fields.file.url, "&embedded=true\"\n      height=\"820\"\n      style=\"width:100%;\"\n      seamless\n      sandbox=\"allow-scripts allow-same-origin\"\n      frameborder=\"0\">\n    </iframe>\n  </div>");
 };
 
 exports["default"] = _default;
