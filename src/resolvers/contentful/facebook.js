fa6ea2628926386f467f6a73799926726d587ee6
ignatospadov <tigeroip45@gmail.com>
2023-02-23 09:42:59 -0500

    updated facebook height

--- src/resolvers/contentful/facebook.js
+++ src/resolvers/contentful/facebook.js
@@ -4,9 +4,9 @@ export default (node) =>
   `<div class="${config.embed.class} ${config.embed.facebook.class}">
     <iframe
       src="https://www.facebook.com/plugins/post.php?href=${encodeURI(node.content)}&width=auto"
-      height="400"
-      style="border:none;overflow:hidden;width:100%"
-      scrolling="no"
+      height="500"
+      style="border:none;overflow:visible;width:100%"
+      scrolling="yes"
       frameborder="0"
       allowfullscreen="true"
       allow="autoplay;
