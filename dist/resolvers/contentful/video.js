3b94ea40ab23d7e9c627d564f44fa1e0ea0cd16a
izhilin <igor.zhilin@cheddar.com>
2023-03-14 18:13:03 -0400

    dist folder

--- dist/resolvers/contentful/video.js
+++ dist/resolvers/contentful/video.js
@@ -10,7 +10,7 @@ var _config = _interopRequireDefault(require("../../config"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var _default = function _default(node) {
-  return "<div\n    class=\"".concat(_config["default"].video["class"], "\"\n    data-caption=\"").concat(node.data.target.fields.description || '', "\"\n    data-isLive=\"").concat(node.data.target.fields.isLive, "\"\n    data-src=\"").concat(node.data.target.fields.videoUrls['application/x-mpegURL'], "\"></div>");
+  return "<div\n    class=\"".concat(_config["default"].video["class"], "\"\n    data-poster=\"https:").concat(node.data.target.fields.thumbnail.fields.file.url, "\"\n    data-isLive=\"").concat(node.data.target.fields.isLive, "\"\n    data-src=\"").concat(node.data.target.fields.videoUrls['application/x-mpegURL'], "\"></div>");
 };
 
 exports["default"] = _default;
