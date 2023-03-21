553f32ba6c35967e083a90d68400f23bf3927fd5
izhilin <igor.zhilin@cheddar.com>
2023-03-21 13:41:03 -0400

    fallback added

--- src/resolvers/contentful/image.js
+++ src/resolvers/contentful/image.js
@@ -1,5 +1,5 @@
 export default (node) =>
   `<div>
     <img src="https:${node?.data?.target?.fields?.file?.url}" alt=""/>
-    <em>${node?.data?.target?.fields?.description}</em>
+    <em>${node?.data?.target?.fields?.description || ''}</em>
   </div>`
