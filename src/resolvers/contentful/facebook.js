7b9b45d7c62364a501d20f67d438a97794e6cbad
izhilin <igor.zhilin@cheddar.com>
2023-04-11 15:35:42 -0400

    facebook script

--- src/resolvers/contentful/facebook.js
+++ src/resolvers/contentful/facebook.js
@@ -2,17 +2,6 @@ import config from '../../config'
 
 export default (node) =>
   `<div class="${config.embed.class} ${config.embed.facebook.class}">
-    <iframe
-      src="https://www.facebook.com/plugins/post.php?href=${encodeURI(node?.content)}&width=auto"
-      data-src="${encodeURI(node?.content)}&width=auto"
-      height="500"
-      style="border:none;overflow:visible;width:100%"
-      scrolling="yes"
-      frameborder="0"
-      allowfullscreen="true"
-      allow="autoplay;
-      clipboard-write;
-      encrypted-media;
-      picture-in-picture;
-      web-share"></iframe>
+    <script async defer src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2"></script>
+    <div class="fb-post" data-href="${encodeURI(node?.content)}"></div>
   </div>`
