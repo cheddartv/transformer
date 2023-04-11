f36a5b7114576bf77f502e48c055c117e3af1f42
izhilin <igor.zhilin@cheddar.com>
2023-04-11 16:15:30 -0400

    facebook script

--- src/resolvers/contentful/facebook.js
+++ src/resolvers/contentful/facebook.js
@@ -2,6 +2,7 @@ import config from '../../config'
 
 export default (node) =>
   `<div class="${config.embed.class} ${config.embed.facebook.class}">
+    <div id="fb-root"></div>
     <script async defer src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2"></script>
-    <div class="fb-post" data-href="${encodeURI(node?.content)}"></div>
+    <div class="fb-post" data-allowfullscreen="true" data-href="${encodeURI(node?.content)}"></div>
   </div>`
