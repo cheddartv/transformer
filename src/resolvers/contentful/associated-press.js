29a34dd1fe8417767c9b30cf8e46c0e34be8b412
izhilin <igor.zhilin@cheddar.com>
2023-04-12 13:43:20 -0400

    AP test

--- src/resolvers/contentful/associated-press.js
+++ src/resolvers/contentful/associated-press.js
@@ -2,5 +2,5 @@ import config from '../../config'
 
 export default (node) => `
   <div class="${config.embed.class} ${config.embed['associated-press'].class}">
-    <iframe src="https://interactives.ap.org/ap-polling-presidential-approval" class="ap-embed" width="100%" height="600" style="border: 1px solid #eee;">
+    <iframe src="https://interactives.ap.org/ap-polling-presidential-approval" class="ap-embed" width="100%" height="600" style="border: 1px solid #eee;"></iframe>
   </div>`
