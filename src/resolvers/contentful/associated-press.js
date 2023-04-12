5aba20efd8c1cf12dd369f09efa2008db6e0b6ec
izhilin <igor.zhilin@cheddar.com>
2023-04-12 12:46:26 -0400

    AP

--- src/resolvers/contentful/associated-press.js
+++ src/resolvers/contentful/associated-press.js
@@ -2,11 +2,5 @@ import config from '../../config'
 
 export default (node) => `
   <div class="${config.embed.class} ${config.embed['associated-press'].class}">
-    <iframe
-      src="${node?.content}"
-      scrolling="no"
-      width="100%"
-      style="border:none"
-      height="400">
-    </iframe>
+    ${node.content}
   </div>`
