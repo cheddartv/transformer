674dec028758e9cf5c9b0c21f0421567250ca498
izhilin <igor.zhilin@cheddar.com>
2023-04-12 14:15:21 -0400

    AP test distro

--- dist/resolvers/contentful/associated-press.js
+++ dist/resolvers/contentful/associated-press.js
@@ -10,7 +10,9 @@ var _config = _interopRequireDefault(require("../../config"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var _default = function _default(node) {
-  return "\n  <div class=\"".concat(_config["default"].embed["class"], " ").concat(_config["default"].embed['associated-press']["class"], "\">\n    ").concat(node.content, "\n  </div>");
+  var src = node.content.match(/<iframe.*?\bsrc="(.*?)".*?>/i);
+  var height = node.content.match(/<iframe.*?\bheight="(.*?)".*?>/i);
+  return "\n    <div class=\"".concat(_config["default"].embed["class"], " ").concat(_config["default"].embed['associated-press']["class"], "\">\n      <iframe\n            src=\"").concat(src[1], "\"\n            height=\"").concat(height[1], "\"\n            style=\"border:0;width:100%;\"\n          </iframe>\n    </div>");
 };
 
 exports["default"] = _default;
--- src/resolvers/contentful/associated-press.js
+++ src/resolvers/contentful/associated-press.js
@@ -1,6 +1,15 @@
 import config from '../../config'
 
-export default (node) => `
-  <div class="${config.embed.class} ${config.embed['associated-press'].class}">
-    ${node.content}
-  </div>`
+export default (node) => {
+  const src = node.content.match(/<iframe.*?\bsrc="(.*?)".*?>/i)
+  const height = node.content.match(/<iframe.*?\bheight="(.*?)".*?>/i)
+
+  return `
+    <div class="${config.embed.class} ${config.embed['associated-press'].class}">
+      <iframe
+            src="${src[1]}"
+            height="${height[1]}"
+            style="border:0;width:100%;"
+          </iframe>
+    </div>`
+}
