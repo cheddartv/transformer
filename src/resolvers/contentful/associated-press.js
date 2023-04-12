372f05d19a851c29604af5b61c719271d20137c4
izhilin <igor.zhilin@cheddar.com>
2023-04-12 13:16:06 -0400

    AP fix

--- src/resolvers/contentful/associated-press.js
+++ src/resolvers/contentful/associated-press.js
@@ -2,7 +2,7 @@ import config from '../../config'
 
 export default (node) => {
 
-  const iframe = node.content.match(/<iframe.*?>/i)
+  const iframe = node.content.match(/<iframe.*?<\/iframe>/i)[0]
 
   return `
   <div class="${config.embed.class} ${config.embed['associated-press'].class}">
