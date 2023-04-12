613d7eb463ef69ce23657895d1d632f2356ea326
izhilin <igor.zhilin@cheddar.com>
2023-04-12 13:59:11 -0400

    AP test distro

--- dist/resolvers/contentful/associated-press.js
+++ dist/resolvers/contentful/associated-press.js
@@ -10,7 +10,14 @@ var _config = _interopRequireDefault(require("../../config"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var _default = function _default(node) {
-  return "\n  <div class=\"".concat(_config["default"].embed["class"], " ").concat(_config["default"].embed['associated-press']["class"], "\">\n    <iframe\n      src=\"").concat(node === null || node === void 0 ? void 0 : node.content, "\"\n      scrolling=\"no\"\n      width=\"100%\"\n      style=\"border:none\"\n      height=\"400\">\n    </iframe>\n  </div>");
+  var src = node.content.match(/<iframe.*?\bsrc="(.*?)".*?>/i) || 'https://interactives.ap.org/ap-polling-presidential-approval';
+  var height = node.content.match(/<iframe.*?\bheight="(.*?)".*?>/i) || 400;
+
+  if (src) {
+    return "\n      <div class=\"".concat(_config["default"].embed["class"], " ").concat(_config["default"].embed['associated-press']["class"], "\">\n        <iframe\n          src=\"").concat(src, "\"\n          scrolling=\"no\"\n          width=\"100%\"\n          style=\"border:1px solid #eee\"\n          height=\"").concat(height, "\">\n        </iframe>\n      </div>");
+  } else {
+    return '';
+  }
 };
 
 exports["default"] = _default;
