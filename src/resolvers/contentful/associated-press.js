c3192ebe3a50a532049da2f9157b3020740b7313
izhilin <igor.zhilin@cheddar.com>
2023-04-12 13:53:10 -0400

    AP test

--- src/resolvers/contentful/associated-press.js
+++ src/resolvers/contentful/associated-press.js
@@ -4,14 +4,18 @@ export default (node) => {
   const src = node.content.match(/<iframe.*?\bsrc="(.*?)".*?>/i)
   const height = node.content.match(/<iframe.*?\bheight="(.*?)".*?>/i) || 400
 
-  return `
-  <div class="${config.embed.class} ${config.embed['associated-press'].class}">
-    <iframe
-      src="${src}"
-      scrolling="no"
-      width="100%"
-      style="border:1px solid #eee"
-      height="${height}">
-    </iframe>
-  </div>`
+  if (src) {
+    return `
+      <div class="${config.embed.class} ${config.embed['associated-press'].class}">
+        <iframe
+          src="${src}"
+          scrolling="no"
+          width="100%"
+          style="border:1px solid #eee"
+          height="${height}">
+        </iframe>
+      </div>`
+  } else {
+    return ''
+  }
 }
