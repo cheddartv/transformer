4fce776b7faeef39cc7ab2c3dddd89cfa63f4a74
izhilin <igor.zhilin@cheddar.com>
2023-04-13 13:53:22 -0400

    facebook distro

--- src/resolvers/contentful/facebook.js
+++ src/resolvers/contentful/facebook.js
@@ -2,5 +2,5 @@ import config from '../../config'
 
 export default (node) =>
   `<div class="${config.embed.class} ${config.embed.facebook.class}">
-    <div class="fb-video" data-allowfullscreen="true" data-width="500" data-href="${encodeURI(node?.content)}"></div>
+    <div class="fb-post" data-allowfullscreen="true" data-width="500" data-href="${encodeURI(node?.content)}"></div>
   </div>`
