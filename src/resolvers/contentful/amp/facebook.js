b2decc7ab7159bd330f4551957b2f8e409673d99
ignatospadov <tigeroip45@gmail.com>
2023-04-14 07:01:40 -0400

    added facebook

--- src/resolvers/contentful/amp/facebook.js
+++ src/resolvers/contentful/amp/facebook.js
@@ -1,17 +1,18 @@
-function type(url) {
-  const regex = /facebook.com\/[a-zA-Z0-9_]+\/(posts|videos)\//
-  const match = url.match(regex)
-  if (match) {
-    return match[1] === 'posts' ? 'post' : 'video'
-  }
-  return 'post'
-}
+import { urlType } from '../facebook'
 
-export default (node) =>
-  `<amp-facebook
-    width="300"
-    height="400"
-    layout="responsive"
-    data-embed-as="${type(node?.content)}"
-    data-href="${node?.content}">
-  </amp-facebook>`
+export default (node) => {
+  const { type, url } = urlType(node?.content)
+
+  if (type === 'unknown') {
+    return ''
+  } else {
+    return `
+      <amp-facebook
+        width="300"
+        height="400"
+        layout="responsive"
+        data-embed-as="${type}"
+        data-href="${url}">
+      </amp-facebook>`
+  }
+}
--- src/resolvers/contentful/facebook.js
+++ src/resolvers/contentful/facebook.js
@@ -1,6 +1,33 @@
 import config from '../../config'
 
-export default (node) =>
-  `<div class="${config.embed.class} ${config.embed.facebook.class}">
-    <div class="fb-post" data-allowfullscreen="true" data-width="500" data-href="${encodeURI(node?.content)}"></div>
-  </div>`
+function postId(url) {
+  const regex = /facebook.com\/([^/]+)\/posts\/([^/]+)/
+  const matches = url.match(regex)
+  return matches ? `https://www.facebook.com/${matches[1]}/posts/${matches[2]}` : null
+}
+
+function videoId(url) {
+  const regex = /facebook.com\/watch\/\?v=([^/]+)/
+  const matches = url.match(regex)
+  return matches ? `https://www.facebook.com/watch/?v=${matches[1]}` : null
+}
+
+export function urlType(url) {
+  if (postId(url)) {
+    return { type: 'post', url: postId(url) }
+  } else if(videoId(url)) {
+    return { type: 'video', url: videoId(url) }
+  } else {
+    return { type: 'unknown', url: null }
+  }
+}
+
+export default (node) =>{
+  const { type, url } = urlType(node?.content)
+
+  if (type === 'unknown') {
+    return ''
+  } else {
+    return `<div class="${config.embed.class} ${config.embed.facebook.class}" data-type="${type}" data-src="${url}"/>`
+  }
+}
