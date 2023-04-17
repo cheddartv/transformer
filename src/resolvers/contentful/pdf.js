981dd1d0894787429d6b97ffc2b4952a8dfe9ff0
ignatospadov <tigeroip45@gmail.com>
2023-04-17 06:51:01 -0400

    added pdf for non amp pages

--- src/resolvers/contentful/pdf.js
+++ src/resolvers/contentful/pdf.js
@@ -1,13 +1,3 @@
 import config from '../../config'
 
-export default (node) =>
-  `<div class="${config.embed.class} ${config.asset.class}">
-    <iframe
-      src="https://docs.google.com/viewerng/viewer?url=https:${node?.data?.target?.fields?.file?.url}&embedded=true"
-      height="820"
-      style="width:100%;"
-      seamless
-      sandbox="allow-scripts allow-same-origin"
-      frameborder="0">
-    </iframe>
-  </div>`
+export default (node) => `<div class="${config.embed.class} ${config.pdf.class}" data-url="https:${encodeURIComponent(node?.data?.target?.fields?.file?.url)}"></div>`
