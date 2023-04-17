ff9ef81aafc3eb0283082ecce270892eb90049a1
ignatospadov <tigeroip45@gmail.com>
2023-04-17 06:54:48 -0400

    removed uri encoding

--- src/resolvers/contentful/pdf.js
+++ src/resolvers/contentful/pdf.js
@@ -1,3 +1,3 @@
 import config from '../../config'
 
-export default (node) => `<div class="${config.embed.class} ${config.pdf.class}" data-url="https:${encodeURIComponent(node?.data?.target?.fields?.file?.url)}"></div>`
+export default (node) => `<div class="${config.embed.class} ${config.pdf.class}" data-url="https:${node?.data?.target?.fields?.file?.url}"></div>`
