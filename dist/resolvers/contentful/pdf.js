5fca893d765168e05c3dac8a54b0d58502804738
ignatospadov <tigeroip45@gmail.com>
2023-04-17 06:51:57 -0400

    build output

--- dist/resolvers/contentful/pdf.js
+++ dist/resolvers/contentful/pdf.js
@@ -12,7 +12,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 var _default = function _default(node) {
   var _node$data, _node$data$target, _node$data$target$fie, _node$data$target$fie2;
 
-  return "<div class=\"".concat(_config["default"].embed["class"], " ").concat(_config["default"].asset["class"], "\">\n    <iframe\n      src=\"https://docs.google.com/viewerng/viewer?url=https:").concat(node === null || node === void 0 ? void 0 : (_node$data = node.data) === null || _node$data === void 0 ? void 0 : (_node$data$target = _node$data.target) === null || _node$data$target === void 0 ? void 0 : (_node$data$target$fie = _node$data$target.fields) === null || _node$data$target$fie === void 0 ? void 0 : (_node$data$target$fie2 = _node$data$target$fie.file) === null || _node$data$target$fie2 === void 0 ? void 0 : _node$data$target$fie2.url, "&embedded=true\"\n      height=\"820\"\n      style=\"width:100%;\"\n      seamless\n      sandbox=\"allow-scripts allow-same-origin\"\n      frameborder=\"0\">\n    </iframe>\n  </div>");
+  return "<div class=\"".concat(_config["default"].embed["class"], " ").concat(_config["default"].pdf["class"], "\" data-url=\"https:").concat(encodeURIComponent(node === null || node === void 0 ? void 0 : (_node$data = node.data) === null || _node$data === void 0 ? void 0 : (_node$data$target = _node$data.target) === null || _node$data$target === void 0 ? void 0 : (_node$data$target$fie = _node$data$target.fields) === null || _node$data$target$fie === void 0 ? void 0 : (_node$data$target$fie2 = _node$data$target$fie.file) === null || _node$data$target$fie2 === void 0 ? void 0 : _node$data$target$fie2.url), "\"></div>");
 };
 
 exports["default"] = _default;
