10aafaadca9bedf35f68f74f05aa39ff04b6c622
izhilin <igor.zhilin@cheddar.com>
2023-06-27 16:40:12 -0400

    lodash unescape

--- src/resolvers/contentful/embed.js
+++ src/resolvers/contentful/embed.js
@@ -1,9 +1,10 @@
 import {video} from '.'
 import config from '../../config'
+import unescape from 'lodash.unescape'
 
 const embed = (node) => {
   if (node?.data?.target?.fields?.type === 'instagram') {
-    return `<div class="instagram-${config.embed.class}"><div>"${node?.data?.target?.fields?.code}"</div></div>`
+    return `<div class="instagram-${config.embed.class}"><div>${unescape(node?.data?.target?.fields?.code)}"</div></div>`
   }
   return `<div class="${config.embed.class}"><div>${node?.data?.target?.fields?.code}</div></div>`
 }
