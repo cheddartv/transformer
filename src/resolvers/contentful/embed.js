765c50760fee3bb923fa2ac788a48bdba7ae1d71
izhilin <igor.zhilin@cheddar.com>
2023-06-27 14:57:07 -0400

    minor fix

--- src/resolvers/contentful/embed.js
+++ src/resolvers/contentful/embed.js
@@ -3,7 +3,8 @@ import config from '../../config'
 
 const embed = (node) => {
   if (node?.data?.target?.fields?.type === 'instagram') {
-    return `<div class="instagram-${config.embed.class}" data-embed="'${node?.data?.target?.fields?.code}'"></div>`
+    const code = node?.data?.target?.fields?.code.replace('"', '&quot;')
+    return `<div class="instagram-${config.embed.class}" data-embed="${code}"></div>`
   }
   return `<div class="${config.embed.class}"><div>${node?.data?.target?.fields?.code}</div></div>`
 }
