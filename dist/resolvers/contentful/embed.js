8f54c7d2ab2e147dcb4844a09f396f8e9a8a456e
izhilin <igor.zhilin@cheddar.com>
2023-06-27 17:23:56 -0400

    html escape

--- dist/resolvers/contentful/embed.js
+++ dist/resolvers/contentful/embed.js
@@ -11,13 +11,17 @@ var _config = _interopRequireDefault(require("../../config"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+var htmlEscape = function htmlEscape(str) {
+  return str.replace(/&/g, '&amp').replace(/'/g, '&apos').replace(/"/g, '&quot').replace(/>/g, '&gt').replace(/</g, '&lt');
+};
+
 var embed = function embed(node) {
   var _node$data, _node$data$target, _node$data$target$fie, _node$data3, _node$data3$target, _node$data3$target$fi;
 
   if ((node === null || node === void 0 ? void 0 : (_node$data = node.data) === null || _node$data === void 0 ? void 0 : (_node$data$target = _node$data.target) === null || _node$data$target === void 0 ? void 0 : (_node$data$target$fie = _node$data$target.fields) === null || _node$data$target$fie === void 0 ? void 0 : _node$data$target$fie.type) === 'instagram') {
     var _node$data2, _node$data2$target, _node$data2$target$fi;
 
-    return "<div class=\"instagram-".concat(_config["default"].embed["class"], "\" data-embed=\"").concat(encodeURI(node === null || node === void 0 ? void 0 : (_node$data2 = node.data) === null || _node$data2 === void 0 ? void 0 : (_node$data2$target = _node$data2.target) === null || _node$data2$target === void 0 ? void 0 : (_node$data2$target$fi = _node$data2$target.fields) === null || _node$data2$target$fi === void 0 ? void 0 : _node$data2$target$fi.code), "\"></div>");
+    return "<div class=\"instagram-".concat(_config["default"].embed["class"], "\" data-embed=\"").concat(htmlEscape(node === null || node === void 0 ? void 0 : (_node$data2 = node.data) === null || _node$data2 === void 0 ? void 0 : (_node$data2$target = _node$data2.target) === null || _node$data2$target === void 0 ? void 0 : (_node$data2$target$fi = _node$data2$target.fields) === null || _node$data2$target$fi === void 0 ? void 0 : _node$data2$target$fi.code), "\"></div>");
   }
 
   return "<div class=\"".concat(_config["default"].embed["class"], "\"><div>").concat(node === null || node === void 0 ? void 0 : (_node$data3 = node.data) === null || _node$data3 === void 0 ? void 0 : (_node$data3$target = _node$data3.target) === null || _node$data3$target === void 0 ? void 0 : (_node$data3$target$fi = _node$data3$target.fields) === null || _node$data3$target$fi === void 0 ? void 0 : _node$data3$target$fi.code, "</div></div>");
--- src/resolvers/contentful/embed.js
+++ src/resolvers/contentful/embed.js
@@ -1,9 +1,18 @@
 import {video} from '.'
 import config from '../../config'
 
+const htmlEscape = (str) => {
+  return str
+    .replace(/&/g, '&amp')
+    .replace(/'/g, '&apos')
+    .replace(/"/g, '&quot')
+    .replace(/>/g, '&gt')
+    .replace(/</g, '&lt');
+}
+
 const embed = (node) => {
   if (node?.data?.target?.fields?.type === 'instagram') {
-    return `<div class="instagram-${config.embed.class}" data-embed="${encodeURI(node?.data?.target?.fields?.code)}"></div>`
+    return `<div class="instagram-${config.embed.class}" data-embed="${htmlEscape(node?.data?.target?.fields?.code)}"></div>`
   }
   return `<div class="${config.embed.class}"><div>${node?.data?.target?.fields?.code}</div></div>`
 }
