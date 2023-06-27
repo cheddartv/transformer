166a811f8329d819d3c1329d464f9b32a7f76308
izhilin <igor.zhilin@cheddar.com>
2023-06-27 14:11:12 -0400

    minor fix

--- dist/resolvers/contentful/embed.js
+++ dist/resolvers/contentful/embed.js
@@ -12,19 +12,21 @@ var _config = _interopRequireDefault(require("../../config"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var embed = function embed(node) {
-  var _node$data, _node$data$target, _node$data$target$fie, _node$data2, _node$data2$target, _node$data2$target$fi;
+  var _node$data, _node$data$target, _node$data$target$fie, _node$data3, _node$data3$target, _node$data3$target$fi;
 
   if ((node === null || node === void 0 ? void 0 : (_node$data = node.data) === null || _node$data === void 0 ? void 0 : (_node$data$target = _node$data.target) === null || _node$data$target === void 0 ? void 0 : (_node$data$target$fie = _node$data$target.fields) === null || _node$data$target$fie === void 0 ? void 0 : _node$data$target$fie.type) === 'instagram') {
-    return "<div class=\"instagram-".concat(_config["default"].embed["class"], "\" data-embed={node?.data?.target?.fields?.code}></div>");
+    var _node$data2, _node$data2$target, _node$data2$target$fi;
+
+    return "<div class=\"instagram-".concat(_config["default"].embed["class"], "\" data-embed=").concat(node === null || node === void 0 ? void 0 : (_node$data2 = node.data) === null || _node$data2 === void 0 ? void 0 : (_node$data2$target = _node$data2.target) === null || _node$data2$target === void 0 ? void 0 : (_node$data2$target$fi = _node$data2$target.fields) === null || _node$data2$target$fi === void 0 ? void 0 : _node$data2$target$fi.code, "></div>");
   }
 
-  return "<div class=\"".concat(_config["default"].embed["class"], "\"><div>").concat(node === null || node === void 0 ? void 0 : (_node$data2 = node.data) === null || _node$data2 === void 0 ? void 0 : (_node$data2$target = _node$data2.target) === null || _node$data2$target === void 0 ? void 0 : (_node$data2$target$fi = _node$data2$target.fields) === null || _node$data2$target$fi === void 0 ? void 0 : _node$data2$target$fi.code, "</div></div>");
+  return "<div class=\"".concat(_config["default"].embed["class"], "\"><div>").concat(node === null || node === void 0 ? void 0 : (_node$data3 = node.data) === null || _node$data3 === void 0 ? void 0 : (_node$data3$target = _node$data3.target) === null || _node$data3$target === void 0 ? void 0 : (_node$data3$target$fi = _node$data3$target.fields) === null || _node$data3$target$fi === void 0 ? void 0 : _node$data3$target$fi.code, "</div></div>");
 };
 
 var _default = function _default(node) {
-  var _node$data3, _node$data3$target, _node$data3$target$sy, _node$data3$target$sy2, _node$data3$target$sy3;
+  var _node$data4, _node$data4$target, _node$data4$target$sy, _node$data4$target$sy2, _node$data4$target$sy3;
 
-  var type = node === null || node === void 0 ? void 0 : (_node$data3 = node.data) === null || _node$data3 === void 0 ? void 0 : (_node$data3$target = _node$data3.target) === null || _node$data3$target === void 0 ? void 0 : (_node$data3$target$sy = _node$data3$target.sys) === null || _node$data3$target$sy === void 0 ? void 0 : (_node$data3$target$sy2 = _node$data3$target$sy.contentType) === null || _node$data3$target$sy2 === void 0 ? void 0 : (_node$data3$target$sy3 = _node$data3$target$sy2.sys) === null || _node$data3$target$sy3 === void 0 ? void 0 : _node$data3$target$sy3.id;
+  var type = node === null || node === void 0 ? void 0 : (_node$data4 = node.data) === null || _node$data4 === void 0 ? void 0 : (_node$data4$target = _node$data4.target) === null || _node$data4$target === void 0 ? void 0 : (_node$data4$target$sy = _node$data4$target.sys) === null || _node$data4$target$sy === void 0 ? void 0 : (_node$data4$target$sy2 = _node$data4$target$sy.contentType) === null || _node$data4$target$sy2 === void 0 ? void 0 : (_node$data4$target$sy3 = _node$data4$target$sy2.sys) === null || _node$data4$target$sy3 === void 0 ? void 0 : _node$data4$target$sy3.id;
 
   switch (type) {
     case 'videoFile':
