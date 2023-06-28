21364d654d542146cfc811d79b34eb5cd37557d5
izhilin <igor.zhilin@cheddar.com>
2023-06-28 16:16:49 -0400

    instagram script is back

--- src/resolvers/contentful/embed.js
+++ src/resolvers/contentful/embed.js
@@ -2,11 +2,17 @@ import { video } from '.'
 import config from '../../config'
 import encodeHTML from '../../util/encodeHTML'
 
-const removeInstagramScript = (html) => html.replace('<script async src="//www.instagram.com/embed.js"></script>', '')
+const fixInstagramScriptSrc = (html) =>
+  html.replace(
+    '<script async src="//www.instagram.com/embed.js"></script>',
+    '<script async src="https://www.instagram.com/embed.js"></script>'
+  )
 
 const embed = (node) =>
   node?.data?.target?.fields?.type === 'instagram'
-    ? `<div class='${config.embed.instagram}' data-embed='${encodeHTML(removeInstagramScript(node?.data?.target?.fields?.code))}'></div>`
+    ? `<div class='${config.embed.instagram}' data-embed='${encodeHTML(
+        fixInstagramScriptSrc(node?.data?.target?.fields?.code)
+      )}'></div>`
     : `<div class='${config.embed.class}'><div>${node?.data?.target?.fields?.code}</div></div>`
 
 export default (node) => {
