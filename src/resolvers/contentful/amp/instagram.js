50f88dedd03660dbeabe40c6bc393fd7f5cecf14
izhilin <igor.zhilin@cheddar.com>
2023-06-29 17:41:20 -0400

    instagram amp distro

--- src/resolvers/contentful/amp/instagram.js
+++ src/resolvers/contentful/amp/instagram.js
@@ -1,5 +1,5 @@
 export default (node) => {
-  const match = node.content.match(/\/(?:p|reel)\/([a-zA-Z0-9_-]+)(?:\/|\?|$)/)
+  const match = node?.data?.target?.fields?.code?.match(/\/(?:p|reel)\/([a-zA-Z0-9_-]+)(?:\/|\?|$)/)
 
   if (match[1]) {
     return `<amp-instagram data-shortcode="${match[1]}" width="350" height="300" layout="responsive"></amp-instagram>`
