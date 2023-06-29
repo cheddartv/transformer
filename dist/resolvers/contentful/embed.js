211112399b809431612afbe7005e9cd48554c7fd
izhilin <igor.zhilin@cheddar.com>
2023-06-29 17:14:03 -0400

    amp

--- dist/resolvers/contentful/embed.js
+++ dist/resolvers/contentful/embed.js
@@ -18,9 +18,17 @@ var fixInstagramScriptSrc = function fixInstagramScriptSrc(html) {
 };
 
 var embed = function embed(node) {
-  var _node$data, _node$data$target, _node$data$target$fie, _node$data2, _node$data2$target, _node$data2$target$fi, _node$data3, _node$data3$target, _node$data3$target$fi;
+  var _node$data, _node$data$target, _node$data$target$fie;
 
-  return (node === null || node === void 0 ? void 0 : (_node$data = node.data) === null || _node$data === void 0 ? void 0 : (_node$data$target = _node$data.target) === null || _node$data$target === void 0 ? void 0 : (_node$data$target$fie = _node$data$target.fields) === null || _node$data$target$fie === void 0 ? void 0 : _node$data$target$fie.type) === 'instagram' ? "<div class='".concat(_config["default"].embed.instagram["class"], "' data-embed='").concat((0, _encodeHTML["default"])(fixInstagramScriptSrc(node === null || node === void 0 ? void 0 : (_node$data2 = node.data) === null || _node$data2 === void 0 ? void 0 : (_node$data2$target = _node$data2.target) === null || _node$data2$target === void 0 ? void 0 : (_node$data2$target$fi = _node$data2$target.fields) === null || _node$data2$target$fi === void 0 ? void 0 : _node$data2$target$fi.code)), "'></div>") : "<div class='".concat(_config["default"].embed["class"], "'><div>").concat(node === null || node === void 0 ? void 0 : (_node$data3 = node.data) === null || _node$data3 === void 0 ? void 0 : (_node$data3$target = _node$data3.target) === null || _node$data3$target === void 0 ? void 0 : (_node$data3$target$fi = _node$data3$target.fields) === null || _node$data3$target$fi === void 0 ? void 0 : _node$data3$target$fi.code, "</div></div>");
+  if (node !== null && node !== void 0 && (_node$data = node.data) !== null && _node$data !== void 0 && (_node$data$target = _node$data.target) !== null && _node$data$target !== void 0 && (_node$data$target$fie = _node$data$target.fields) !== null && _node$data$target$fie !== void 0 && _node$data$target$fie.code.startsWith('<blockquote class="instagram-media"')) {
+    var _node$data2, _node$data2$target, _node$data2$target$fi;
+
+    return "<div class='".concat(_config["default"].embed.instagram["class"], "' \n                 data-embed='").concat((0, _encodeHTML["default"])(fixInstagramScriptSrc(node === null || node === void 0 ? void 0 : (_node$data2 = node.data) === null || _node$data2 === void 0 ? void 0 : (_node$data2$target = _node$data2.target) === null || _node$data2$target === void 0 ? void 0 : (_node$data2$target$fi = _node$data2$target.fields) === null || _node$data2$target$fi === void 0 ? void 0 : _node$data2$target$fi.code)), "'></div>");
+  } else {
+    var _node$data3, _node$data3$target, _node$data3$target$fi;
+
+    return "<div class='".concat(_config["default"].embed["class"], "'><div>").concat(node === null || node === void 0 ? void 0 : (_node$data3 = node.data) === null || _node$data3 === void 0 ? void 0 : (_node$data3$target = _node$data3.target) === null || _node$data3$target === void 0 ? void 0 : (_node$data3$target$fi = _node$data3$target.fields) === null || _node$data3$target$fi === void 0 ? void 0 : _node$data3$target$fi.code, "</div></div>");
+  }
 };
 
 var _default = function _default(node) {
