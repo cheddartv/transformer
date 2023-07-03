527353ff99f11ab49398c4eb6c003938fb7c85bd
izhilin <igor.zhilin@cheddar.com>
2023-07-03 14:14:53 -0400

    decode html export

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,12 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+Object.defineProperty(exports, "decodeHTML", {
+  enumerable: true,
+  get: function get() {
+    return _decodeHTML["default"];
+  }
+});
 Object.defineProperty(exports, "parse", {
   enumerable: true,
   get: function get() {
@@ -11,5 +17,7 @@ Object.defineProperty(exports, "parse", {
 });
 
 var _transformer = _interopRequireDefault(require("./transformer"));
+
+var _decodeHTML = _interopRequireDefault(require("./decodeHTML"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
