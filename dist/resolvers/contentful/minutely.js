aaad6d006536344e5cd1d9fdca23bd8acafea186
izhilin <igor.zhilin@cheddar.com>
2023-07-28 13:11:30 -0400

    embedId/accountId

--- dist/resolvers/contentful/minutely.js
+++ dist/resolvers/contentful/minutely.js
@@ -10,8 +10,10 @@ var _config = _interopRequireDefault(require("../../config"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var _default = function _default(node) {
-  var embedId = node.content.match(/.*embed_id=([^&|\n|\t\s]+)/) || null;
-  var accountId = node.content.match(/-news12-([^\/]+)\/editor\/([^\/]+)/) || null;
+  var _node$content, _node$content$match, _node$content2, _node$content2$match;
+
+  var embedId = ((_node$content = node.content) === null || _node$content === void 0 ? void 0 : (_node$content$match = _node$content.match(/.*embed_id=([^&|\n|\t\s]+)/)) === null || _node$content$match === void 0 ? void 0 : _node$content$match[1]) || null;
+  var accountId = ((_node$content2 = node.content) === null || _node$content2 === void 0 ? void 0 : (_node$content2$match = _node$content2.match(/-news12-([^\/]+)\/editor\/([^\/]+)/)) === null || _node$content2$match === void 0 ? void 0 : _node$content2$match[1]) || null;
 
   if (!embedId || !accountId) {
     return '';
--- src/resolvers/contentful/minutely.js
+++ src/resolvers/contentful/minutely.js
@@ -1,8 +1,8 @@
 import config from '../../config'
 
 export default (node) => {
-  const embedId = node.content.match(/.*embed_id=([^&|\n|\t\s]+)/) || null
-  const accountId = node.content.match(/-news12-([^\/]+)\/editor\/([^\/]+)/) || null
+  const embedId = node.content?.match(/.*embed_id=([^&|\n|\t\s]+)/)?.[1] || null
+  const accountId = node.content?.match(/-news12-([^\/]+)\/editor\/([^\/]+)/)?.[1] || null
 
   if (!embedId || !accountId) {
     return ''
