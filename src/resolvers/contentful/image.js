668dd35f26ff20b462040931e0f3ffa9a755645b
izhilin <igor.zhilin@cheddar.com>
2023-08-02 12:33:20 -0400

    add class-name to image wrapper div

--- src/resolvers/contentful/image.js
+++ src/resolvers/contentful/image.js
@@ -1,5 +1,7 @@
+import config from '../../config'
+
 export default (node) =>
-  `<div>
+  `<div class="${config.image.class}">
     <img src="https:${node?.data?.target?.fields?.file?.url}?fit=fill&w=1920&h=1080&q=85" alt=""/>
     <em>${node?.data?.target?.fields?.description || ''}</em>
   </div>`
