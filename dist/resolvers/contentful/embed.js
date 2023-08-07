faa8d9f53f98392a15f2262b23351ae2bde89bbc
izhilin <igor.zhilin@cheddar.com>
2023-08-07 14:47:16 -0400

    minor fix

--- dist/resolvers/contentful/embed.js
+++ dist/resolvers/contentful/embed.js
@@ -26,7 +26,7 @@ var embed = function embed(node) {
     case codeSnippet.startsWith('<blockquote class="instagram-media"'):
       return "<div class='".concat(_config["default"].embed.instagram["class"], "' \n                 data-embed='").concat((0, _he.encode)(fixInstagramScriptSrc(node === null || node === void 0 ? void 0 : (_node$data2 = node.data) === null || _node$data2 === void 0 ? void 0 : (_node$data2$target = _node$data2.target) === null || _node$data2$target === void 0 ? void 0 : (_node$data2$target$fi = _node$data2$target.fields) === null || _node$data2$target$fi === void 0 ? void 0 : _node$data2$target$fi.code)), "'></div>");
 
-    case codeSnippet.startsWith('<script type="text/javascript" src="https://portal.cityspark.com/PortalScripts/news12-'):
+    case codeSnippet.includes('https://portal.cityspark.com/PortalScripts/news12'):
       return "<div class='".concat(_config["default"].embed.citySpark["class"], "' \n                 data-embed='").concat(node === null || node === void 0 ? void 0 : (_node$data3 = node.data) === null || _node$data3 === void 0 ? void 0 : (_node$data3$target = _node$data3.target) === null || _node$data3$target === void 0 ? void 0 : (_node$data3$target$fi = _node$data3$target.fields) === null || _node$data3$target$fi === void 0 ? void 0 : _node$data3$target$fi.code, "'></div>");
 
     default:
