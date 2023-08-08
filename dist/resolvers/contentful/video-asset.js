7f5e417a696f1984fe7112a648e77b388c9ad96a
izhilin <igor.zhilin@cheddar.com>
2023-08-08 13:40:11 -0400

    description field added

--- dist/resolvers/contentful/video-asset.js
+++ dist/resolvers/contentful/video-asset.js
@@ -10,7 +10,7 @@ var _config = _interopRequireDefault(require("../../config"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var _default = function _default(node) {
-  return "<div\n    class=\"".concat(_config["default"]['video-asset']["class"], "\"\n    data-title=\"").concat(node.data.target.fields.file.title, "\"\n    data-type=\"").concat(node.data.target.fields.file.contentType, "\"\n    data-src=\"https://").concat(node.data.target.fields.file.url, "#t=0.001\"></div>");
+  return "<div\n    class=\"".concat(_config["default"]['video-asset']["class"], "\"\n    data-title=\"").concat(node.data.target.fields.file.title, "\"\n    data-type=\"").concat(node.data.target.fields.file.contentType, "\"\n    data-description=\"").concat(node.data.target.fields.description, "\"\n    data-src=\"https://").concat(node.data.target.fields.file.url, "#t=0.001\"></div>");
 };
 
 exports["default"] = _default;
