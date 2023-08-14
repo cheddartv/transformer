a3ce564f1d34a91a0e5fdecc3f9becc8cd988b82
Maryna <maryna.humeniuk@sigma.software>
2023-08-14 15:20:40 +0300

    feat(tests): refactored tests

--- src/resolvers/contentful/embed.js
+++ src/resolvers/contentful/embed.js
@@ -32,20 +32,20 @@ const embed = (node) => {
 }
 
 const list = (node) => {
-  return `<div class=${config.embed.storyList.class}'>${node?.data?.target?.fields?.stories
+  return `<div class="${config.embed.storyList.class}">${node?.data?.target?.fields?.stories
     ?.map((story) => {
       const hls = story?.fields?.videoFile?.fields?.videoUrls?.['application/x-mpegURL']
       const mp4 = story?.fields?.videoFile?.fields?.videoUrls?.['video/mp4']
       const duration = story?.fields?.videoFile?.fields?.duration
       const thumbnail = story?.fields?.thumbnail?.fields?.file?.url
 
-      return `<div class='story' data-title='${story?.fields?.title}'
-                     data-slug='${story?.fields?.slug}' 
-                     data-published='${story?.fields?.publishedAt}'
-                     ${thumbnail ? `data-thumb='${thumbnail}'` : ''} 
-                     ${mp4 ? `data-mp4='${mp4}'` : ''}
-                     ${hls ? `data-hls='${hls}'` : ''}
-                     ${duration ? `data-duration='${duration}'` : ''}>
+      return `<div class="story" data-title="${story?.fields?.title}"
+                     data-slug="${story?.fields?.slug}" 
+                     data-published="${story?.fields?.publishedAt}"
+                     ${thumbnail ? `data-thumb="${thumbnail}"` : ''} 
+                     ${mp4 ? `data-mp4="${mp4}"` : ''}
+                     ${hls ? `data-hls="${hls}"` : ''}
+                     ${duration ? `data-duration="${duration}"` : ''}>
                   </div>`
     })
     .join('')}</div>`
