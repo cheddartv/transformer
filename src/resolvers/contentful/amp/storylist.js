c218fab16c2061a5a854e436b2143325bd7cabb6
Maryna <maryna.humeniuk@sigma.software>
2023-08-15 12:51:06 +0300

    feat(storylist): added data for amp

--- src/resolvers/contentful/amp/storylist.js
+++ src/resolvers/contentful/amp/storylist.js
@@ -1,11 +1,11 @@
 export default (node) => {
   return `<div class="story-list">
-  <p>${node?.data?.target?.fields?.name || 'Related Stories'}</p>
+  <p class="story-list-title">${node?.data?.target?.fields?.name || 'Related Stories'}</p>
   ${node?.data?.target?.fields?.stories
     ?.map(
       (story) => `<div class="story">
                           <a className='story' href=${story?.fields?.slug}>
-                            <span className='timestamp'>{${story?.fields?.publishedAt}}</span>
+                            <span className='timestamp'>${story?.fields?.publishedAt}</span>
                             <p className='title'>${story?.fields?.title}</p>
                           </a>
                         </div>`
