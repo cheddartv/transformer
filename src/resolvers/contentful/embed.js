54bb2e60693e9142439347ebce79d3a243cfbf3b
izhilin <igor.zhilin@cheddar.com>
2023-08-23 15:46:35 -0400

    quotes fix

--- src/resolvers/contentful/embed.js
+++ src/resolvers/contentful/embed.js
@@ -54,18 +54,18 @@ const list = (node) => {
 }
 
 const platformsList = (node) => {
-  return `<div class=${config.embed.platformsList.class}'>
+  return `<div class="${config.embed.platformsList.class}">
   <p>${node?.data?.target?.fields?.name || 'Where To Watch'}</p>
   ${node?.data?.target?.fields?.platforms
     ?.map(
-      (platform) => `<div class='platform' data-name='${platform?.fields?.name}'
-                     data-url='${platform?.fields?.url}' 
-                     data-background='${platform?.fields?.background}'
-                     data-backgroundHover='${platform?.fields?.backgroundColorHover}'
-                     data-logo='${platform?.fields?.logo?.fields?.file?.url}'
-                     data-logoTitle='${platform?.fields?.logo?.fields?.title || platform?.fields?.logo?.fields?.description}'
-                     data-logoHover='${platform?.fields?.logoHover?.fields?.file?.url}'
-                     data-logoHover='${platform?.fields?.logoHover?.fields?.file?.url}'>
+      (platform) => `<div class="platform" data-name="${platform?.fields?.name}"
+                     data-url="${platform?.fields?.url}"
+                     data-background="${platform?.fields?.background}"
+                     data-backgroundHover="${platform?.fields?.backgroundColorHover}"
+                     data-logo="${platform?.fields?.logo?.fields?.file?.url}"
+                     data-logoTitle="${platform?.fields?.logo?.fields?.title || platform?.fields?.logo?.fields?.description}"
+                     data-logoHover="${platform?.fields?.logoHover?.fields?.file?.url}"
+                     data-logoHover="${platform?.fields?.logoHover?.fields?.file?.url}">
                   </div>`
     )
     .join('')}</div>`
