d8474e72046482475b3668dd22ac75606c04d318
izhilin <igor.zhilin@cheddar.com>
2023-08-24 16:18:43 -0400

    amp pages support

---
+++ src/resolvers/contentful/amp/platformsList.js
@@ -0,0 +1,16 @@
+import config from '../../../config'
+
+export default (node) => {
+  return `<div class="${config.embed.platformsList.class}">
+    <p class="title">${node?.data?.target?.fields?.name || 'Where To Watch'}</p>
+    <div class="platforms-container">
+    ${node?.data?.target?.fields?.platforms
+      ?.map((platform) => {
+        return `<a class="platform-link" "${platform?.fields?.url}">
+                                <div class="platform">
+                                  <img src="${platform?.fields?.logo?.fields?.file?.url}" alt="${platform?.fields?.logo?.fields?.title}">
+                                </div>
+                              </a>`
+      })
+      .join('')}</div></div>`
+}
