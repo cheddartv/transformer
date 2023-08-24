6662ed8879058830afc93a4e0ee1797b61ae4c1c
izhilin <igor.zhilin@cheddar.com>
2023-08-24 16:52:47 -0400

    refactor

--- src/resolvers/contentful/amp/platformsList.js
+++ src/resolvers/contentful/amp/platformsList.js
@@ -2,15 +2,17 @@ import config from '../../../config'
 
 export default (node) => {
   return `<div class="${config.embed.platformsList.class}">
-    <p class="platforms-title">${node?.data?.target?.fields?.name || 'Where To Watch'}</p>
-    <div class="platforms-container">
-    ${node?.data?.target?.fields?.platforms
-      ?.map((platform) => {
-        return `<a class="platform-link" "${platform?.fields?.url}">
-                                <div class="platform" style="background: ${platform?.fields?.background}">
-                                  <img src="https:${platform?.fields?.logo?.fields?.file?.url}" alt="${platform?.fields?.logo?.fields?.title}">
-                                </div>
-                              </a>`
-      })
-      .join('')}</div></div>`
+            <p class="platforms-title">${node?.data?.target?.fields?.name || "Where To Watch"}</p>
+            <div class="platforms-container">
+            ${node?.data?.target?.fields?.platforms
+              ?.map((platform) => {
+                return `<a class="platform-link" href="${platform?.fields?.url}">
+                          <div class="platform" style="background: ${platform?.fields?.background}">
+                            <img src="https:${platform?.fields?.logo?.fields?.file?.url}" alt="${platform?.fields?.logo?.fields?.title}">
+                          </div>
+                        </a>`
+              })
+              .join(``)}
+            </div>
+          </div>`
 }
