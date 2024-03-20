9b6e40e527deb577a11698f4d7e036a04ee1d75a
Maryna Kravets <gum225@gmail.com>
2024-03-20 14:07:56 +0200

    feat [31547]: prevent undefined in the url for poster video embed

--- src/resolvers/contentful/video.js
+++ src/resolvers/contentful/video.js
@@ -1,8 +1,12 @@
 import config from '../../config'
 
+
+const FALLBACK_IMAGE_URL =
+  '//images-cf.news12static.com/3kqcuzntcg31/3npmffTlb8qXpqzrYLWXTq/c59543c81cc66265ca2bf4ebb4c7edb8/News12_1920x1080_v2.jpg'
+
 export default (node) =>
   `<div
     class="${config.video.class}"
-    data-poster="https:${node?.data?.target?.fields?.thumbnail?.fields?.file?.url}?fit=fill&w=1920&h=1080&q=85"
+    data-poster="https:${node?.data?.target?.fields?.thumbnail?.fields?.file?.url ?? FALLBACK_IMAGE_URL}?fit=fill&w=1920&h=1080&q=85"
     data-isLive="${node?.data?.target?.fields?.isLive}"
     data-src="${node?.data?.target?.fields?.videoUrls?.['application/x-mpegURL']}"></div>`
